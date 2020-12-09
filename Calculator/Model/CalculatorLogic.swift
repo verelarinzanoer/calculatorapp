366df4fbf32941100b75eb3e20b42fc8984128f0
verelarinzanoer <68377120+verelarinzanoer@users.noreply.github.com>
2020-12-09 10:01:19 +0700

    Add files via upload

---
+++ Calculator/Model/CalculatorLogic.swift
@@ -0,0 +1,60 @@
+//
+//  CalculatorLogic.swift
+//  Calculator
+//
+//  Created by Apple on 20/11/20.
+//  Copyright ©️ 2020 London App Brewery. All rights reserved.
+//
+
+import Foundation
+
+
+struct CalculatorLogic {
+    private var number: Double?
+    
+    private var intermediateCalculation: (n1: Double, calcMethod: String)?
+    
+    mutating func setNumber(_ number: Double){
+        self.number = number
+    }
+    
+    mutating func calculate(symbol: String) -> Double? {
+        
+        if let n = number{
+            switch symbol {
+            case "+/-":
+                return n * -1
+            case "AC":
+                return 0
+            case "%":
+                return n * 0.01
+            case "=":
+                return perfomTwoNumCalculation(n2: n)
+            default:
+                intermediateCalculation = (n1: n, calcMethod: symbol)
+            }
+        }
+        
+        return nil
+    }
+    
+    private func perfomTwoNumCalculation(n2: Double) -> Double?{
+        
+        if let n1 = intermediateCalculation?.n1, let operation = intermediateCalculation?.calcMethod{
+            switch operation {
+            case "+":
+                return n1 + n2
+            case "-":
+                return n1 - n2
+            case "×":
+                return n1 * n2
+            case "÷":
+                return n1 / n2
+            default:
+                fatalError("Error")
+            }
+        }
+        
+        return nil
+    }
+}
